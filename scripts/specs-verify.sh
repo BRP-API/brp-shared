3420e2bf3ef8e879412c8a972bbdc710bd4aaa4f
Melvin Lee <melvin.lee@iswish.nl>
2024-09-26 08:37:06 +0200

    chore(automation): fout bij veld met waarde null in response gefixed

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -4,4 +4,6 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
                 features/docs \
-                --tags "not @integratie"
+                --tags "not @integratie" \
+                --tags "not @skip-verify"
+
