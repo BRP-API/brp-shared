64a6839640fccc37515c5991b5b1f89f69249fa4
Melvin Lee <melvin.lee@iswish.nl>
2024-12-17 15:32:27 +0100

    chore(specs-verify): aanroep volgorde aangepast tbv gebruik als referentie implementatie

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "### Cucumber Test Reports"
+
+echo "#### Step definitions validatie"
+
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
@@ -8,7 +12,5 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 --tags "not @skip-verify" \
                 > /dev/null
 
-echo "### Cucumber Test Reports"
-echo "#### Step definitions validatie"
 node ./scripts/generate-step-summary.js ./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt ./test-reports/cucumber-js/step-summary.txt
 cat ./test-reports/cucumber-js/step-summary.txt
