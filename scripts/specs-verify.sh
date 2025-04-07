1db06457e0201413a5f21a18c7a03446193e633d
nkorporaal <nap.korporaal+github@gmail.com>
2025-04-07 10:45:46 +0200

    ci: disable wip features

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
-                -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
-                -f summary \
-                features/docs \
-                -p UnitTest \
-                > /dev/null
+# npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
+#                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
+#                 -f summary \
+#                 features/docs \
+#                 -p UnitTest \
+#                 > /dev/null
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-integratie-summary.txt \
@@ -14,19 +14,19 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 -p Integratie \
                 > /dev/null
 
-npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-informatie-api.json \
-                -f summary:./test-reports/cucumber-js/step-definitions/test-result-informatie-api-summary.txt \
-                -f summary \
-                features/docs \
-                -p InfoApi \
-                > /dev/null
+# npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-informatie-api.json \
+#                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-informatie-api-summary.txt \
+#                 -f summary \
+#                 features/docs \
+#                 -p InfoApi \
+#                 > /dev/null
 
-npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-data-api.json \
-                -f summary:./test-reports/cucumber-js/step-definitions/test-result-data-api-summary.txt \
-                -f summary \
-                features/docs \
-                -p DataApi \
-                > /dev/null
+# npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-data-api.json \
+#                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-data-api-summary.txt \
+#                 -f summary \
+#                 features/docs \
+#                 -p DataApi \
+#                 > /dev/null
 
 # npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-gezag-api.json \
 #                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-gezag-api-summary.txt \
