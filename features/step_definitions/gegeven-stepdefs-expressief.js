6e1e796202e3c71935018b6fcfe3a95fee40dd50
fsamwel <37143495+fsamwel@users.noreply.github.com>
2025-04-22 15:43:58 +0200

    bij opgeven geboortedatum ook datum ingang familierechtelijke betrekking aanpassen

--- features/step_definitions/gegeven-stepdefs-expressief.js
+++ features/step_definitions/gegeven-stepdefs-expressief.js
@@ -227,10 +227,21 @@ Given(/^is geboren in het buitenland/, function () {
 });
 
 Given(/^(?:'(.*)' )?is geboren op (\d*)-(\d*)-(\d*)$/, function (aanduiding, dag, maand, jaar) {
+    let persoon = getPersoon(this.context, aanduiding);
+    let geboortedatum = toBRPDate(dag, maand, jaar)
+
+    if (persoon.hasOwnProperty('ouder-1') && persoon['ouder-1'].at(-1).familie_betrek_start_datum == persoon.persoon.at(-1).geboorte_datum) {
+        persoon['ouder-1'].at(-1).familie_betrek_start_datum = geboortedatum
+    }
+
+    if (persoon.hasOwnProperty('ouder-2') && persoon['ouder-2'].at(-1).familie_betrek_start_datum == persoon.persoon.at(-1).geboorte_datum) {
+        persoon['ouder-2'].at(-1).familie_betrek_start_datum = geboortedatum
+    }
+
     aanvullenPersoon(
         getPersoon(this.context, aanduiding),
         arrayOfArraysToDataTable([
-            ['geboortedatum (03.10)', toBRPDate(dag, maand, jaar)]
+            ['geboortedatum (03.10)', geboortedatum]
         ])
     );
 });
