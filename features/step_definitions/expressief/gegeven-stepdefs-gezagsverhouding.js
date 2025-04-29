317393c4d3d741348532dffa1f11986bf9941e1c
fsamwel <37143495+fsamwel@users.noreply.github.com>
2025-04-29 08:54:07 +0200

    gerechtelijke uitspraak meerdere varianten met verschillende varianten datumformaten

--- features/step_definitions/expressief/gegeven-stepdefs-gezagsverhouding.js
+++ features/step_definitions/expressief/gegeven-stepdefs-gezagsverhouding.js
@@ -1,6 +1,6 @@
 const { Given } = require('@cucumber/cucumber');
 const { getPersoon } = require('../contextHelpers');
-const { createGezagsverhouding, wijzigGezagsverhouding, aanvullenGezagsverhouding } = require('../persoon-2');
+const { createGezagsverhouding, aanvullenGezagsverhouding } = require('../persoon-2');
 const { arrayOfArraysToDataTable } = require('../dataTableFactory');
 
 Given(/^voor '(.*)' is een gerechtelijke uitspraak over het gezag gedaan met de volgende gegevens$/, function (aanduiding, dataTable) {
@@ -55,7 +55,7 @@ Given(/^in een gerechtelijke uitspraak is het gezag toegewezen aan '(.*)'$/, fun
     )
 });
 
-function uitspraakGezagOpDatum(relatieveDatum, aanduiding) {
+function uitspraakGezagEenOuderOpDatum(relatieveDatum, aanduiding) {
     let indicatieGezag = getIndicatieGezag(this.context, aanduiding);
 
     createGezagsverhouding(
@@ -67,8 +67,8 @@ function uitspraakGezagOpDatum(relatieveDatum, aanduiding) {
     )
 }
 
-Given('{dd-mm-yyyy datum} is in een gerechtelijke uitspraak het gezag toegewezen aan {string}', uitspraakGezagOpDatum);
-Given('{vandaag, gisteren of morgen x jaar geleden} is in een gerechtelijke uitspraak het gezag toegewezen aan {string}', uitspraakGezagOpDatum);
+Given('{dd-mm-yyyy datum} is in een gerechtelijke uitspraak het gezag toegewezen aan {string}', uitspraakGezagEenOuderOpDatum);
+Given('{vandaag, gisteren of morgen x jaar geleden} is in een gerechtelijke uitspraak het gezag toegewezen aan {string}', uitspraakGezagEenOuderOpDatum);
 
 Given(/^in een gerechtelijke uitspraak is het gezag toegewezen aan '(.*)' en een derde$/, function (aanduiding) {
     let indicatieGezag = (getIndicatieGezag(this.context, aanduiding) == IndicatieGezag.Ouder1)
@@ -115,18 +115,18 @@ Given(/^in een gerechtelijke uitspraak is het gezag toegewezen aan beide ouders$
     )
 });
 
-Given(/^(.*) is in een gerechtelijke uitspraak het gezag toegewezen aan beide ouders$/, function (relatieveDatum) {
+Given(/^in een gerechtelijke uitspraak is het gezag toegewezen aan beide ouders$/, function () {
     let indicatieGezag = IndicatieGezag.BeideOuders;
 
     createGezagsverhouding(
         getPersoon(this.context, undefined),
         arrayOfArraysToDataTable([
             ['indicatie gezag minderjarige (32.10)', indicatieGezag],
-            ['datum ingang geldigheid (85.10)', relatieveDatum],
+            ['datum ingang geldigheid (85.10)', 'gisteren - 1 jaar'],
         ])
     )
 });
-
+    
 function uitspraakGezagBeideOudersOpDatum(relatieveDatum) {
     let indicatieGezag = IndicatieGezag.BeideOuders;
 
@@ -167,14 +167,14 @@ function uitspraakGezagDerdeOpDatum(relatieveDatum) {
 }
 
 Given('{dd-mm-yyyy datum} is in een gerechtelijke uitspraak het gezag toegewezen aan een voogdijinstelling', uitspraakGezagDerdeOpDatum);
-Given('{vandaag, gisteren of morgen x jaar geleden} is in een gerechtelijke uitspraak het gezag toegewezen aan een voogdijinstelling', uitspraakGezagDerdeOpDatum);
 Given('{dd-mm-yyyy datum} is in een gerechtelijke uitspraak het gezag toegewezen aan een derde', uitspraakGezagDerdeOpDatum);
+Given('{vandaag, gisteren of morgen x jaar geleden} is in een gerechtelijke uitspraak het gezag toegewezen aan een voogdijinstelling', uitspraakGezagDerdeOpDatum);
 Given('{vandaag, gisteren of morgen x jaar geleden} is in een gerechtelijke uitspraak het gezag toegewezen aan een derde', uitspraakGezagDerdeOpDatum);
 
 function getIndicatieGezag(context, aanduiding) {
     let plKind = getPersoon(context, undefined);
-    let plKindOuder1 = plKind['ouder-1'];
-    let plKindOuder2 = plKind['ouder-2'];
+    let plKindOuder1 = plKind['ouder-1'].filter((voorkomen) => voorkomen.volg_nr == 0);
+    let plKindOuder2 = plKind['ouder-2'].filter((voorkomen) => voorkomen.volg_nr == 0);
 
     if (plKindOuder1) {
         plKindOuder1 = plKindOuder1[0];
