40943a9b07ee5a6024b4942b2167dbf406ad6ea9
Melvin Lee <melvin.lee@iswish.nl>
2025-11-24 17:17:57 +0100

    chore: fixes nav sync naar personen info api repo

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo "### Cucumber Test Reports"
+
+EXIT_CODE=0
+
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
                 features/docs \
                 -p UnitTest \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-integratie.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-integratie-summary.txt \
@@ -13,6 +18,7 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 features/docs \
                 -p Integratie \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-informatie-api.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-informatie-api-summary.txt \
@@ -20,6 +26,7 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 features/docs \
                 -p InfoApi \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-data-api.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-data-api-summary.txt \
@@ -27,6 +34,7 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 features/docs \
                 -p DataApi \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-gezag-api.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-gezag-api-summary.txt \
@@ -34,6 +42,7 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 features/docs \
                 -p GezagApi \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
 
 npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-gezag-api-deprecated.json \
                 -f summary:./test-reports/cucumber-js/step-definitions/test-result-gezag-api-deprecated-summary.txt \
@@ -41,3 +50,6 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 features/docs \
                 -p GezagApiDeprecated \
                 > /dev/null
+if [[ $? -ne 0 ]]; then EXIT_CODE=1; fi
+
+exit $EXIT_CODE
